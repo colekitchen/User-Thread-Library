2eef75b4649885c40c5ba3315427a78ed643c639
Cole Austin Kitchen <cakitch@ada5.computing.clemson.edu>
2026-02-26 10:58:57 -0500

    updates 1

--- threadmanagement.c
+++ threadmanagement.c
@@ -5,44 +5,70 @@
 #include <stdio.h>
 #include "mythreads.h"
 
-ucontext_t *threads;
-char *stacks;
+struct Thread {
+    ucontext_t thread;
+    void *stack;
+    int status;
+};
+
+ucontext_t main_context;
 void **returnValues;
-int current_thread;
+struct Thread *threads;
+int current_thread, recent_thread, numOfThreads;
 
 void threadStubFunction(thFuncPtr funcPtr, void *argPtr) {
+    threads[current_thread].status = 0;
     void *returnValue = funcPtr(argPtr);
-    returnValues = realloc(returnValues, current_thread * sizeof(void *));
+    if (numOfThreads != 0) {
+        returnValues = realloc(returnValues, (numOfThreads+1) * sizeof(void *));
+        returnValues[numOfThreads] = NULL;
+    }
     returnValues[current_thread] = returnValue;
-    
+
+    swapcontext(&threads[current_thread].thread, &main_context);
 }
 
 void threadInit(void) {
-    threads = malloc(1 * sizeof(ucontext_t));
-    stacks = malloc(1 * STACK_SIZE);
-    returnValues = malloc(1 * sizeof(void *));
+    threads = malloc(1 * sizeof(struct Thread));
+    returnValues = calloc(1, sizeof(void *));
+    numOfThreads = 0;
+    recent_thread = 0;
     current_thread = 0;
     return;
 }
+
 int threadCreate(thFuncPtr funcPtr, void *argPtr) {
-    if (current_thread != 0) {
-        threads = realloc(threads, current_thread * sizeof(ucontext_t));
-        stacks = (char *)realloc(stacks, current_thread * STACK_SIZE);
+    if (numOfThreads != 0) {
+        threads = realloc(threads, (numOfThreads+1) * sizeof(struct Thread));
     }
-    if (getcontext(&threads[current_thread]) == -1) {
+    if (getcontext(&threads[numOfThreads].thread) == -1) {
         perror("Failed to get context");
     }
 
-    threads[current_thread].uc_stack.ss_sp = stacks[current_thread];
-	threads[current_thread].uc_stack.ss_size = STACK_SIZE;
+    threads[numOfThreads].stack = malloc(STACK_SIZE);
+    threads[numOfThreads].thread.uc_stack.ss_sp = threads[numOfThreads].stack;
+	threads[numOfThreads].thread.uc_stack.ss_size = STACK_SIZE;
 
-    makecontext(&threads[current_thread], (void (*)())threadStubFunction, 2, funcPtr, argPtr);
+    makecontext(&threads[numOfThreads].thread, (void (*)(void))threadStubFunction, 2, funcPtr, argPtr);
 
-    current_thread++;
+    current_thread = numOfThreads;
 
-    return current_thread;
+    swapcontext(&main_context, &threads[numOfThreads].thread);
+
+    threads[current_thread].status = 1;
+
+    return numOfThreads++;
 }
 void threadYield(void) {
+    recent_thread = current_thread;
+
+    for (int i = 0; i < numOfThreads+1; i++) {
+        if (threads[i].status == 0 && i != recent_thread) {
+            current_thread = i;
+            break;
+        }
+    }
+    swapcontext(&threads[recent_thread].thread, &threads[current_thread].thread);
     return;
 }
 void threadJoin(int thread_id, void **result) {
